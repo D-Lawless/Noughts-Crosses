59b9d75b770a756c6e8214c0c0c26c173ebab1f3
cd <12>
2020-02-21 21:10:12 +0000

    Comments Added

--- src/main/java/BoardValidator.java
+++ src/main/java/BoardValidator.java
@@ -1,5 +1,6 @@
 public class BoardValidator {
 
+    //The following function checks for the winner of the game(including a tie), if the game is still ongoing or if the board is invalid.
     public static String checkWinnerWithChecker(char[][] currentGame) {
 
         int xCount = 0;
@@ -12,8 +13,10 @@ public class BoardValidator {
         final String invalidBoard = "Invalid Board";
         final String tieGameStr = "Game is a tie";
 
+        //This loop allows us to look through the board for the placed 'X's and 'O's
         for (int i = 0; i < 3; i++) {
 
+            //This nested for loop allows count number of times 'X' or 'O'
             for (int j = 0; j < 3; j++) {
 
                 if (currentGame[i][j] == 'X') {
@@ -25,6 +28,7 @@ public class BoardValidator {
 
             }
 
+            //The following checks the rows for 3 matching
             if (currentGame[i][0] == currentGame[i][1] && currentGame[i][0] == currentGame[i][2]) {
                 if (currentGame[i][0] == 'X') {
                     xWinner = true;
@@ -34,6 +38,7 @@ public class BoardValidator {
                 }
             }
 
+            //The following checks the columns for 3 matching
             if (currentGame[0][i] == currentGame[1][i] && currentGame[0][i] == currentGame[2][i]) {
                 if (currentGame[0][i] == 'X') {
                     xWinner = true;
@@ -44,11 +49,12 @@ public class BoardValidator {
             }
         }
 
+        //This allows us to check if the board is invalid by comparing the amount
         if (xCount > oCount + 1 || xCount < oCount) {
             return invalidBoard;
         }
 
-
+        // The following checks the boards places diagonally
         if (currentGame[0][0] == currentGame[1][1] && currentGame[0][0] == currentGame[2][2]) {
             if (currentGame[0][0] == 'X') {
                 xWinner = true;
@@ -58,6 +64,7 @@ public class BoardValidator {
             }
         }
 
+        // The following checks the boards places diagonally in the opposite direction
         if (currentGame[2][0] == currentGame[1][1] && currentGame[0][0] == currentGame[0][2]) {
             if (currentGame[0][0] == 'X') {
                 xWinner = true;
@@ -67,57 +74,57 @@ public class BoardValidator {
             }
         }
 
+
         if (xWinner) {
             return xWinnerStr;
         } else if (oWinner) {
             return oWinnerStr;
         }
 
-
+        //Checks if 9 ties are on the board returning a tie output
         if (xCount + oCount == 9) {
             return tieGameStr;
         }
 
+        //If none of the above have caused the function to complete we can consider the game still ongoing.
         return notFinishedStr;
 
     }
 
+    //The following function checks for the winner of the game and if the game is still ongoing.
+    //Although this function is a simplified version it has a lower time complexity.
     public static String checkWinner(char[][] currentGame) {
 
-        int xCount = 0;
-        int oCount = 0;
+
         boolean xWinner = false;
         boolean oWinner = false;
         final String xWinnerStr = "X is the Winner!";
         final String oWinnerStr = "O is the Winner!";
         final String notFinishedStr = "Game yet to finish";
-        final String invalidBoard = "invalidBoard";
 
+        //This loop allows us to look through the board for the placed 'X's and 'O's
         for (int i = 0; i < 3; i++) {
-
-            if (xCount > oCount + 1 || xCount < oCount) {
-                return invalidBoard;
-            }
-
+            //The following checks the rows for 3 matching
             if (currentGame[i][0] == currentGame[i][1] && currentGame[i][0] == currentGame[i][2]) {
                 if (currentGame[i][0] == 'X') {
-                    return xWinnerStr;
+                    xWinner = true;
                 }
                 if (currentGame[i][0] == 'O') {
-                    return oWinnerStr;
+                    oWinner = true;
                 }
             }
-
+            //The following checks the columns for 3 matching
             if (currentGame[0][i] == currentGame[1][i] && currentGame[0][i] == currentGame[2][i]) {
                 if (currentGame[0][i] == 'X') {
-                    return xWinnerStr;
+                    xWinner = true;
                 }
                 if (currentGame[0][i] == 'O') {
-                    return oWinnerStr;
+                    oWinner = true;
                 }
             }
         }
 
+        // The following checks the boards places diagonally
         if (currentGame[0][0] == currentGame[1][1] && currentGame[0][0] == currentGame[2][2]) {
             if (currentGame[0][0] == 'X') {
                 xWinner = true;
@@ -127,6 +134,7 @@ public class BoardValidator {
             }
         }
 
+        // The following checks the boards places diagonal in the opposite direction
         if (currentGame[2][0] == currentGame[1][1] && currentGame[0][0] == currentGame[0][2]) {
             if (currentGame[0][0] == 'X') {
                 xWinner = true;
@@ -142,6 +150,7 @@ public class BoardValidator {
             return oWinnerStr;
         }
 
+        //If none of the above have caused the function to complete we can consider the game still ongoing.
         return notFinishedStr;
 
     }
